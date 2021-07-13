1382bfa8cfe4319c7d40071352460a4e5bea957c
Daniel Reynolds <danielreynolds101@gmail.com>
2021-07-13 14:13:09 -0600

    create more components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Grid } from '@material-ui/core';
 import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
     return (
@@ -10,6 +11,9 @@ function App() {
             <Grid container>
                 <Grid item xs={12}>
                     <Header/>
+                </Grid>
+                <Grid item xs={12}>
+                    <Main/>
                 </Grid>
             </Grid>
         </div>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Grid, Paper } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    main: {
+        
+    },
+    downloadSection: {
+        margin: "40px"
+    }
+});
 
 export default function Main() {
-    
+    const classes = useStyles();
+    return <Container maxWidth="md" className={classes.main}>
+        <Paper className={classes.downloadSection}>
+            
+        </Paper>
+    </Container>
 }
