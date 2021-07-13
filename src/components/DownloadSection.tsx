3d026373eb1771accacb4801279196459793cfe7
Daniel Reynolds <danielreynolds101@gmail.com>
2021-07-13 15:10:14 -0600

    county autoselector

--- src/components/DownloadSection.tsx
+++ src/components/DownloadSection.tsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import { Container, Grid, Paper } from '@material-ui/core';
+import { Container, Grid, Paper, TextField } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
+import counties from '../json/counties.json'
 
 const useStyles = makeStyles({
-    
+
 });
 
-export default function Main() {
-    const [selectedCounty, setSelectedCounty] = useState("");
+export default function DownloadSection() {
     const classes = useStyles();
-    return <>
+    const countiesSorted = counties.sort((countyA, countyB) => Number(countyA.GISJOIN.substring(1, countyA.GISJOIN.length)) - Number(countyB.GISJOIN.substring(1, countyB.GISJOIN.length)));
+    const [selectedCounty, setSelectedCounty] = useState(countiesSorted[0]);
 
+    return <>
+        <Autocomplete
+            options={countiesSorted}
+            value={selectedCounty}
+            onChange={(event, newValue) => {
+                if (newValue) {
+                    setSelectedCounty(newValue)
+                }
+            }}
+            autoHighlight
+            getOptionLabel={(option) => option.name}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="Choose a county"
+                    variant="outlined"
+                    inputProps={{
+                        ...params.inputProps,
+                        autoComplete: 'new-password', // disable autocomplete and autofill
+                    }}
+                />
+            )}
+        />
     </>
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import { Container, Grid, Paper } from '@material-ui/core';
+import DownloadSection from "./DownloadSection";
 
 const useStyles = makeStyles({
     main: {
@@ -15,7 +16,7 @@ export default function Main() {
     const classes = useStyles();
     return <Container maxWidth="md" className={classes.main}>
         <Paper className={classes.downloadSection}>
-            
+            <DownloadSection/>
         </Paper>
     </Container>
 }
