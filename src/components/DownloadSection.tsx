eadbca2470a717f9fc40e518844f83f523229221
Daniel Reynolds <danielreynolds101@gmail.com>
2021-07-13 14:37:47 -0600

    added counites

---
+++ src/components/DownloadSection.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Grid, Paper } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    
+});
+
+export default function Main() {
+    const [selectedCounty, setSelectedCounty] = useState("");
+    const classes = useStyles();
+    return <>
+
+    </>
+}
