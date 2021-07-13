41815af68a60056b3a0ec3b05177443c36ba323d
Daniel Reynolds <danielreynolds101@gmail.com>
2021-07-13 14:03:11 -0600

    header kinda done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Grid } from '@material-ui/core';
+import Header from './components/Header';
 
 function App() {
     return (
         <div className="App">
-            
+            <Grid container>
+                <Grid item xs={12}>
+                    <Header/>
+                </Grid>
+            </Grid>
         </div>
     );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    header: {
+        background: "cyan",
+        height: "80px",
+        borderRadius: "0px",
+        textAlign: "left"
+    },
+    headerTitle: {
+        padding: "10px"
+    }
+});
 
 export default function Header() {
-    
+    const classes = useStyles();
+    return <Paper className={classes.header}>
+        <Typography variant="h3" className={classes.headerTitle}>
+            Sustain Download Service
+        </Typography>
+    </Paper>
 }
