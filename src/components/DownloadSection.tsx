09e427508ec24cdde52fe647c164b4c1d6e534d5
Daniel Reynolds <danielreynolds101@gmail.com>
2021-07-13 15:44:34 -0600

    autocomplete for collection

--- src/components/DownloadSection.tsx
+++ src/components/DownloadSection.tsx
@@ -3,17 +3,33 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Container, Grid, Paper, TextField } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 import counties from '../json/counties.json'
+import { useEffect } from "react";
 
 const useStyles = makeStyles({
-
+    root: {
+        padding: "25px"
+    }
 });
 
-export default function DownloadSection() {
+export default React.memo(function DownloadSection() {
     const classes = useStyles();
     const countiesSorted = counties.sort((countyA, countyB) => Number(countyA.GISJOIN.substring(1, countyA.GISJOIN.length)) - Number(countyB.GISJOIN.substring(1, countyB.GISJOIN.length)));
     const [selectedCounty, setSelectedCounty] = useState(countiesSorted[0]);
+    const [menumetadata, setMenumetadata] = useState([] as any[])
+    const [selectedDataset, setSelectedDataset] = useState({} as any);
 
-    return <>
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/Project-Sustain/aperture-client/master/src/json/menumetadata.json').then(r => r.json())
+            .then(data => { setMenumetadata(data); setSelectedDataset(data[0]) })
+            .catch(e => console.error("Booo"))
+    }, [])
+
+
+    if(!menumetadata.length){
+        return null;
+    }
+
+    return <div className={classes.root}>
         <Autocomplete
             options={countiesSorted}
             value={selectedCounty}
@@ -36,5 +52,30 @@ export default function DownloadSection() {
                 />
             )}
         />
-    </>
-}
+
+        <br/>
+
+        <Autocomplete
+            options={menumetadata}
+            value={selectedDataset}
+            onChange={(event, newValue) => {
+                if (newValue) {
+                    setSelectedDataset(newValue)
+                }
+            }}
+            autoHighlight
+            getOptionLabel={(option) => option.label ?? option.collection}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="Choose a dataset"
+                    variant="outlined"
+                    inputProps={{
+                        ...params.inputProps,
+                        autoComplete: 'new-password', // disable autocomplete and autofill
+                    }}
+                />
+            )}
+        />
+    </div>
+});
