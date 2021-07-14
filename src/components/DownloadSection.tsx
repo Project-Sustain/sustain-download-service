b0238adc973e28421adf618367e313a94778d8c6
Daniel Reynolds <danielreynolds101@gmail.com>
2021-07-14 12:33:42 -0600

    heavy refactor

--- src/components/DownloadSection.tsx
+++ src/components/DownloadSection.tsx
@@ -61,7 +61,6 @@ END OF TERMS AND CONDITIONS
 import React, { useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import { Container, Grid, Paper, TextField, Typography, Tooltip, Button, Switch } from '@material-ui/core';
-import { Autocomplete } from '@material-ui/lab';
 import counties from '../json/counties.json'
 import { useEffect } from "react";
 import Util from '../library/apertureUtil'
@@ -71,141 +70,74 @@ import HourglassEmptyIcon from '@material-ui/icons/HourglassEmpty';
 import LinkIcon from '@material-ui/icons/Link';
 import { isLinked } from "../library/DatasetUtil";
 import Download from "../library/Download";
+import DownloadSetup from "./DownloadSetup"
+
+type downloadStateType = "setup" | "downloading" | "doneSuccess" | "doneFail" | "doneEmpty"
 
 const useStyles = makeStyles({
     root: {
         padding: "25px"
-    },
-    tagsContainer: {
-        margin: "10px"
-    },
-    tag: {
-        float: "left"
     }
 });
 
 export default React.memo(function DownloadSection() {
     const classes = useStyles();
     const countiesSorted = counties.sort((countyA, countyB) => Number(countyA.GISJOIN.substring(1, countyA.GISJOIN.length)) - Number(countyB.GISJOIN.substring(1, countyB.GISJOIN.length)));
-    const [selectedCounty, setSelectedCounty] = useState(countiesSorted[0]);
     const [menumetadata, setMenumetadata] = useState([] as any[])
-    const [selectedDataset, setSelectedDataset] = useState({} as any);
-    const [includeGeospatialData, setIncludeGeospatialData] = useState(false)
+    const [downloadState, setDownloadState] = useState("setup" as downloadStateType)
 
     useEffect(() => {
         fetch('https://raw.githubusercontent.com/Project-Sustain/aperture-client/master/src/json/menumetadata.json').then(r => r.json())
-            .then(data => { setMenumetadata(data); setSelectedDataset(data[0]) })
+            .then(data => { setMenumetadata(data); })
             .catch(e => console.error("Booo"))
     }, []);
 
-    const getTags = () => {
-        let tags = []
-        if (selectedDataset.temporal) {
-            tags.push(makeTag("This dataset is temporal, and will have multiple records per entry.", <HourglassEmptyIcon/>))
+    const conductDownload = async (selectedDataset: any, GISJOIN: string, includeGeospatialData: boolean): Promise<void> => {
+        setDownloadState("downloading")
+        try {
+            const {data, geometry} = await Download(selectedDataset, GISJOIN, includeGeospatialData);
+            if(data.length){
+                setDownloadState("doneSuccess")
+            }
+            else{
+                setDownloadState("doneEmpty")
+            }
         }
-        if (isLinked(selectedDataset)) {
-            tags.push(makeTag("This dataset does not come with geospatial data by default, this can be changed under the 'include geospatial data' option.", <ExploreOffIcon />))
-        }
-        else {
-            tags.push(makeTag("This dataset will come with geospatial data, and will be packaged as a GeoJSON Feature array.", <ExploreIcon />))
-        }
-        if(isLinked(selectedDataset) && includeGeospatialData){
-            tags.push(makeTag("A seperate file containing geospatial information as a GeoJSON Feature array will be included.", <LinkIcon />))
-        }
-        return tags;
-    }
-
-    const makeTag = (tooltipContent: string, icon: JSX.Element) => {
-        return <Tooltip title={<Typography>{tooltipContent}</Typography>} key={tooltipContent}>
-            {icon}
-        </Tooltip>
-    }
-
-    const renderLinkOption = () => {
-        if (!isLinked(selectedDataset)) {
-            return null;
-        }
-        return <>
-            <Typography align="left">Include Geospatial Data</Typography>
-            <Switch
-                color="primary"
-                checked={includeGeospatialData}
-                onChange={e => setIncludeGeospatialData(e.target.checked)}
-            />
-        </>
+        catch (e) {
+            console.error(e)
+            setDownloadState("doneFail")
+        }   
     }
 
     if (!menumetadata.length) {
         return null;
     }
+    
+    const renderBasedOnDownloadState = () => {
+        if(downloadState === "setup"){
+            return <DownloadSetup conductDownload={conductDownload} menumetadata={menumetadata} countiesSorted={countiesSorted}/>
+        }
+        else if(downloadState === "downloading"){
+
+        }
+        else {
+            renderDone();
+        }
+    }
+
+    const renderDone = () => {
+        if(downloadState === "doneSuccess"){
+
+        }
+        else if(downloadState === "doneEmpty"){
+
+        }
+        else if(downloadState === "doneFail"){
+
+        }
+    }
 
     return <div className={classes.root}>
-        <Autocomplete
-            options={countiesSorted}
-            value={selectedCounty}
-            onChange={(event, newValue) => {
-                if (newValue) {
-                    setSelectedCounty(newValue)
-                }
-            }}
-            autoHighlight
-            getOptionLabel={(option) => option.name}
-            renderInput={(params) => (
-                <TextField
-                    {...params}
-                    label="Choose a county"
-                    variant="outlined"
-                    inputProps={{
-                        ...params.inputProps,
-                        autoComplete: 'new-password', // disable autocomplete and autofill
-                    }}
-                />
-            )}
-        />
-
-        <br />
-
-        <Autocomplete
-            options={menumetadata}
-            value={selectedDataset}
-            onChange={(event, newValue) => {
-                if (newValue) {
-                    setSelectedDataset(newValue)
-                }
-            }}
-            autoHighlight
-            getOptionLabel={(option) => option.label ?? Util.cleanUpString(option.collection)}
-            renderInput={(params) => (
-                <TextField
-                    {...params}
-                    label="Choose a dataset"
-                    variant="outlined"
-                    inputProps={{
-                        ...params.inputProps,
-                        autoComplete: 'new-password', // disable autocomplete and autofill
-                    }}
-                />
-            )}
-        />
-
-        <br />
-
-        {renderLinkOption()}
-
-        <br />
-
-        <Typography align="left">Tags</Typography>
-        <div className={classes.tagsContainer}>
-            {getTags()}
-        </div>
-
-        <br />
-
-        <Button variant="contained" color="primary" onClick={async () => {
-            const d = await Download(selectedDataset, selectedCounty.GISJOIN, includeGeospatialData);
-            console.log({d})
-        }}>
-            Download Data
-        </Button>
+        {renderBasedOnDownloadState()}
     </div>
 });
---
+++ src/components/DownloadSetup.tsx
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import { Container, Grid, Paper, TextField, Typography, Tooltip, Button, Switch } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
+import Util from '../library/apertureUtil'
+import ExploreOffIcon from '@material-ui/icons/ExploreOff';
+import ExploreIcon from '@material-ui/icons/Explore';
+import HourglassEmptyIcon from '@material-ui/icons/HourglassEmpty';
+import LinkIcon from '@material-ui/icons/Link';
+import { isLinked } from "../library/DatasetUtil";
+import { makeStyles } from '@material-ui/core/styles';
+
+interface downloadSetupProps {
+    countiesSorted: any[],
+    menumetadata: any[],
+    conductDownload: (selectedDataset: any, GISJOIN: string, includeGeospatialData: boolean) => Promise<void>
+}
+
+const useStyles = makeStyles({
+    tagsContainer: {
+        margin: "10px"
+    },
+    tag: {
+        float: "left"
+    }
+});
+
+export default function DownloadSetup({ countiesSorted, menumetadata, conductDownload }: downloadSetupProps) {
+    const classes = useStyles();
+    const [includeGeospatialData, setIncludeGeospatialData] = useState(false)
+    const [selectedCounty, setSelectedCounty] = useState(countiesSorted[0]);
+    const [selectedDataset, setSelectedDataset] = useState(menumetadata[0]);
+
+    const getTags = () => {
+        let tags = []
+        if (selectedDataset.temporal) {
+            tags.push(makeTag("This dataset is temporal, and will have multiple records per entry.", <HourglassEmptyIcon />))
+        }
+        if (isLinked(selectedDataset)) {
+            tags.push(makeTag("This dataset does not come with geospatial data by default, this can be changed under the 'include geospatial data' option.", <ExploreOffIcon />))
+        }
+        else {
+            tags.push(makeTag("This dataset will come with geospatial data, and will be packaged as a GeoJSON Feature array.", <ExploreIcon />))
+        }
+        if (isLinked(selectedDataset) && includeGeospatialData) {
+            tags.push(makeTag("A seperate file containing geospatial information as a GeoJSON Feature array will be included.", <LinkIcon />))
+        }
+        return tags;
+    }
+
+    const makeTag = (tooltipContent: string, icon: JSX.Element) => {
+        return <Tooltip title={<Typography>{tooltipContent}</Typography>} key={tooltipContent}>
+            {icon}
+        </Tooltip>
+    }
+
+    const renderLinkOption = () => {
+        if (!isLinked(selectedDataset)) {
+            return null;
+        }
+        return <>
+            <Typography align="left">Include Geospatial Data</Typography>
+            <Switch
+                color="primary"
+                checked={includeGeospatialData}
+                onChange={e => setIncludeGeospatialData(e.target.checked)}
+            />
+        </>
+    }
+
+    return <>
+        <Autocomplete
+            options={countiesSorted}
+            value={selectedCounty}
+            onChange={(event, newValue) => {
+                if (newValue) {
+                    setSelectedCounty(newValue)
+                }
+            }}
+            autoHighlight
+            getOptionLabel={(option) => option.name}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="Choose a county"
+                    variant="outlined"
+                    inputProps={{
+                        ...params.inputProps,
+                        autoComplete: 'new-password', // disable autocomplete and autofill
+                    }}
+                />
+            )}
+        />
+
+        <br />
+
+        <Autocomplete
+            options={menumetadata}
+            value={selectedDataset}
+            onChange={(event, newValue) => {
+                if (newValue) {
+                    setSelectedDataset(newValue)
+                }
+            }}
+            autoHighlight
+            getOptionLabel={(option) => option.label ?? Util.cleanUpString(option.collection)}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="Choose a dataset"
+                    variant="outlined"
+                    inputProps={{
+                        ...params.inputProps,
+                        autoComplete: 'new-password', // disable autocomplete and autofill
+                    }}
+                />
+            )}
+        />
+
+        <br />
+
+        {renderLinkOption()}
+
+        <br />
+
+        <Typography align="left">Tags</Typography>
+        <div className={classes.tagsContainer}>
+            {getTags()}
+        </div>
+
+        <br />
+
+        <Button variant="contained" color="primary" onClick={() => conductDownload(selectedDataset, selectedCounty.GISJOIN, includeGeospatialData)}>
+            Download Data
+        </Button>
+    </>
+}
