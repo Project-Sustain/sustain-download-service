4f05eb63e37bff798779a3da28c27f1fb848c774
Daniel Reynolds <danielreynolds101@gmail.com>
2021-07-27 11:56:29 -0600

    set up promise

--- src/library/DownloadUtil.ts
+++ src/library/DownloadUtil.ts
@@ -3,12 +3,23 @@ export const getApiKey = () => {
     return urlParams.get('api_key');
 }
 
+interface downloadCheckType {
+    canDownload: boolean,
+    previousDownloadTime?: number
+}
+
 export const checkIfCanDownload = async (apiKey: string) => {
-    return new Promise<boolean>(() => {
-        fetch(`https://urban-sustain.org/api/download?apiKey=${apiKey}`).then(async function(response) {
+    return new Promise<downloadCheckType>((resolve) => {
+        fetch(`https://urban-sustain.org/api/download?apiKey=${apiKey}`).then(async function (response) {
             console.log(response)
             const body = await response.text();
-            console.log({body})
+            console.log({ body })
+            if (response.status === 200) {
+                resolve({ canDownload: true })
+            }
+            else {
+                resolve({ canDownload: false, previousDownloadTime: new Date(body).valueOf() })
+            }
         });
     })
 }
