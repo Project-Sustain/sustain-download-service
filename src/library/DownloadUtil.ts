d992bed5decaa51610c658cd11cf64e74d5f30eb
Daniel Reynolds <danielreynolds101@gmail.com>
2021-07-27 11:47:21 -0600

    change

--- src/library/DownloadUtil.ts
+++ src/library/DownloadUtil.ts
@@ -6,7 +6,6 @@ export const getApiKey = () => {
 export const checkIfCanDownload = async (apiKey: string) => {
     return new Promise<boolean>(() => {
         fetch(`https://urban-sustain.org/api/download?apiKey=${apiKey}`).then(async function(response) {
-            console.log(`status: ${response}`)
             console.log(response)
             const body = await response.text();
             console.log({body})
