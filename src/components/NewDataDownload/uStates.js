c0586a230bd45da6d3c324217f0c18715390a0a9
Matt Young <honeybadgerofdoom@gmail.com>
2021-09-30 15:54:48 -0600

    fix fill on hover

--- src/components/NewDataDownload/uStates.js
+++ src/components/NewDataDownload/uStates.js
@@ -64,8 +64,7 @@ import {uStatePaths} from "./StateInfo";
 export function Draw(id, setSelectedState, setHoveredState, toolTip){
 
     function mouseOver(event){
-        d3.select("#tooltip").transition().duration(200).style("fill", "#747ED6");
-        // d3.select("#tooltip").transition().duration(200).style("opacity", .9);
+        d3.select("#tooltip").transition().duration(200).style("opacity", .9);
         d3.select("#tooltip").html(setHoveredState(event.target.getAttribute("stateName")));
 
         //FIXME Get this to work
@@ -75,22 +74,12 @@ export function Draw(id, setSelectedState, setHoveredState, toolTip){
     }
 
     function mouseOut(){
-        d3.select("#tooltip").transition().duration(500).style("fill", "#747ED6");
-        // d3.select("#tooltip").transition().duration(500).style("opacity", 0);
+        d3.select("#tooltip").transition().duration(500).style("opacity", 0);
     }
-
-    const niceBlue = "#747ED6";
-    const niceGrey = "#919190";
 
     d3.select(id).selectAll(".state")
         .data(uStatePaths).enter().append("path").attr("class","state").attr("d",function(state){ return state.statePath;})
         .attr("stateName",function(state){ return state.stateName;})
         .on("click", function(state){return setSelectedState(state.target.attributes.stateName.nodeValue)})
         .on("mouseover", mouseOver).on("mouseout", mouseOut);
-    // d3.select(id).selectAll(".state")
-    //     .data(uStatePaths).enter().append("path").attr("class","state").attr("d",function(state){ return state.statePath;})
-    //     .attr("stateName",function(state){ return state.stateName;})
-    //     .on("click", function(state){return setSelectedState(state.target.attributes.stateName.nodeValue)})
-    //     .style("fill", "#747ED6")
-    //     .on("mouseover", mouseOver).on("mouseout", mouseOut);
 }
