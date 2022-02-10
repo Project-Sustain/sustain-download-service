c697f411bf1a01e31cf1378a01de60695490242f
kmbear3 <kmbarram@gmail.com>
2022-02-10 16:33:06 -0700

    change dangerArray to the 4 epa datasets

--- src/components/Utils/DangerData.js
+++ src/components/Utils/DangerData.js
@@ -1,57 +1,6 @@
 export const dangerArray = [
-    "covid_county",
-    "neon_barometric_pressure",
-    "neon_2d_wind",
-    "neon_single_asp_air_temperature",
-    "neon_triple_asp_air_temperature",
-    "neon_ir_bio_temp",
-    "neon_secondary_precipitation",
-    "neon_throughfall_precipitation",
-    "neon_photosynthetically_active_radiation",
-    "neon_shortwave_longwave_radiation_net",
-    "neon_shortwave_radiation",
-    "neon_soil_heat_flux_plate",
-    "neon_photosynthetically_active_radiation",
-    "neon_soil_co2_concentration",
-    "neon_soil_water_content_and_water_salinity",
-    "neon_relative_humidity",
-    "epa_aqi_pm25",
-    "epa_aqi_lead_tsp",
-    "epa_aqi_lead_pm10",
-    "epa_aqi_pm10_total",
     "epa_aqi_ozone",
     "epa_aqi_nitrogen",
     "epa_aqi_carbon_monoxide",
     "epa_aqi_sulfur_dioxide",
-    "epa_aqi_general",
-    "flood_zones_geo"
 ]
-
-
-// covid_county
-// neon_barometric_pressure
-// neon_2d_wind
-// neon_single_asp_air_temperature
-// neon_triple_asp_air_temperature
-// neon_ir_bio_temp
-// neon_secondary_precipitation
-// neon_throughfall_precipitation
-//
-// neon_photosynthetically_active_radiation
-// neon_shortwave_longwave_radiation_net
-// neon_shortwave_radiation
-// neon_soil_heat_flux_plate
-// neon_photosynthetically_active_radiation
-// neon_soil_co2_concentration
-// neon_soil_water_content_and_water_salinity
-// neon_relative_humidity
-//
-// epa_aqi_pm25
-// epa_aqi_lead_tsp
-// epa_aqi_lead_pm10
-// epa_aqi_pm10_total
-// epa_aqi_ozone
-// epa_aqi_nitrogen
-// epa_aqi_carbon_monoxide
-// epa_aqi_sulfur_dioxide
-// epa_aqi_general
